85ff6e487511c6721cdb126f6ebd0863ce511194
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-09-17 23:11:16 -0300

    Update for blog post

--- src/main/java/dev/matheuscruz/product/ProductCreatedHandler.java
+++ src/main/java/dev/matheuscruz/product/ProductCreatedHandler.java
@@ -2,17 +2,24 @@ package dev.matheuscruz.product;
 
 import io.dapr.Topic;
 import io.dapr.client.domain.CloudEvent;
+import io.dapr.client.domain.State;
+import io.dapr.utils.TypeRef;
+import io.quarkiverse.dapr.core.SyncDaprClient;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Path("/handlers")
 @ApplicationScoped
 public class ProductCreatedHandler {
+
+
+    @Inject
+    SyncDaprClient syncDaprClient;
 
     List<CloudEvent<String>> events = new ArrayList<>();
 
@@ -22,9 +29,27 @@ public class ProductCreatedHandler {
     public void handle(CloudEvent<String> event) {
         System.out.println("Received: " + event.getData());
         events.add(event);
+        try {
+            State<Product> state = syncDaprClient.getState("kvstore", event.getData(), TypeRef.get(Product.class));
+            System.out.println("We already have a product with name: " + state.getValue());
+        } catch (Exception e) {
+            System.out.println("We do not have a product :(");
+            // Let's create
+            syncDaprClient.saveState("kvstore", event.getData(), new Product(
+                    event.getData()
+            ));
+        }
     }
 
+    /**
+     * Just to have a way to get events!
+     * @return
+     */
+    @GET
+    @Path("/events")
     public List<CloudEvent<String>> getEvents() {
         return Collections.unmodifiableList(events);
     }
+
+    public record Product(String name) {}
 }
--- src/main/java/dev/matheuscruz/product/ProductResource.java
+++ src/main/java/dev/matheuscruz/product/ProductResource.java
@@ -2,9 +2,13 @@ package dev.matheuscruz.product;
 
 import io.quarkiverse.dapr.core.SyncDaprClient;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.Random;
 
 @Path("/products")
 public class ProductResource {
@@ -14,13 +18,11 @@ public class ProductResource {
     @Inject
     SyncDaprClient syncDaprClient;
 
-    public ProductResource() {
-    }
-
     @POST
     public Response create() {
-        syncDaprClient.publishEvent(DEV_SERVICE_PUBSUBE_NAME, "products.new", "New product created");
+        List<String> products = List.of("mouse", "book", "pencil");
+        int random = new Random().nextInt(products.size());
+        syncDaprClient.publishEvent(DEV_SERVICE_PUBSUBE_NAME, "products.new", products.get(random));
         return Response.accepted().build();
     }
-
 }
